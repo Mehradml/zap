615ff44e5e955dba45d3848f60974eb83f087715
Mehrad Malayeri <mehrad.malayeri@gmail.com>
2022-02-01 01:07:54 +0330

    https://github.com/project-chip/zap/issues/208

--- src-electron/validation/validation.js
+++ src-electron/validation/validation.js
@@ -172,6 +172,11 @@ function checkBoundsFloat(defaultValue, min, max) {
   return defaultValue >= min && defaultValue <= max
 }
 
+function isValidSignedNumberString(value) {
+  return /^(0x)?[\dA-F]+$/i.test(value) || Number.isInteger(Number(value));
+}
+
+
 // exports
 exports.validateAttribute = validateAttribute
 exports.validateEndpoint = validateEndpoint
